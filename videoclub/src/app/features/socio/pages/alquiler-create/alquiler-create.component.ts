e1fb2bdf1ec2a4a9d149ca6f70708f662a4a9534
JaviLatorre <162303283+JaviLatorre@users.noreply.github.com>
2025-11-27 20:43:34 +0100

    creados componentes

---
+++ videoclub/src/app/features/socio/pages/alquiler-create/alquiler-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alquiler-create',
+  imports: [],
+  templateUrl: './alquiler-create.component.html',
+  styleUrl: './alquiler-create.component.scss'
+})
+export class AlquilerCreateComponent {
+
+}
---
+++ videoclub/src/app/features/socio/pages/socio-login/socio-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-socio-login',
+  imports: [],
+  templateUrl: './socio-login.component.html',
+  styleUrl: './socio-login.component.scss'
+})
+export class SocioLoginComponent {
+
+}
