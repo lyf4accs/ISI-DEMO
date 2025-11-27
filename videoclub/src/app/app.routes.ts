b147b28b9734ce9777a2dde6e2062c474538f15b
Laura Junli Illan Ferrandez <LJILLFER@upv.edu.es>
2025-11-27 20:26:08 +0100

    app.routes.ts

--- videoclub/src/app/app.routes.ts
+++ videoclub/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ADMIN_ROUTES } from './features/admin/admin.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    children: ADMIN_ROUTES,
+  },
+  // rutas socio ...
+  { path: '', redirectTo: '/admin', pathMatch: 'full' },
+];
